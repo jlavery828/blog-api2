ebda9573de2a4970bdc480d1092a09d6f31be18a
Jason <jlavery828@gmail.com>
2025-07-28 14:12:47 -0400

    removed static files in urlpatterns

--- BlogBackend/urls.py
+++ BlogBackend/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
