dfb723b142494102aebae27b11e0e6e176952f76
Jason <jlavery828@gmail.com>
2025-07-29 12:38:20 -0400

    added creds to boto-script

--- boto-script.py
+++ boto-script.py
@@ -1,10 +1,34 @@
+import os
 import boto3
 import logging
 
-# Enable logging
-logging.basicConfig(filename='boto_test.log', level=logging.DEBUG)
 
-s3 = boto3.client('s3', endpoint_url='https://nyc3.digitaloceanspaces.com')
+# Enable logging globally for boto3, botocore, s3transfer
+logging.basicConfig(
+    filename="boto_debug.log",
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s"
+)
 
+# Explicitly enable logging for boto3 and botocore
+logging.getLogger("boto3").setLevel(logging.DEBUG)
+logging.getLogger("botocore").setLevel(logging.DEBUG)
+logging.getLogger("s3transfer").setLevel(logging.DEBUG)
+
+# Optional: log to console too
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+logging.getLogger().addHandler(console)
+
+# Create an S3 client (DigitalOcean Spaces-compatible)
+s3 = boto3.client(
+    "s3",
+    region_name="nyc3",
+    endpoint_url="https://codetitan.nyc3.digitaloceanspaces.com",
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+)
+
+# Make a simple API call to trigger logging
 response = s3.list_buckets()
 print(response)
