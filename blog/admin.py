860f68c98cc44df3a48d7bd09d59a1da25988f6f
Jason <jlavery828@gmail.com>
2025-07-31 14:25:01 -0400

    added category to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, PostImage
+from .models import Category, Post, PostImage
 
 
+admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(PostImage)
