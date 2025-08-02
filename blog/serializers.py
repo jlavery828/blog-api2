e3d2bc03b154398210379e942c16e9135733ac2b
Jason <jlavery828@gmail.com>
2025-08-02 14:47:18 -0400

    added instance.save() to update() on PostSerializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -42,6 +42,17 @@ class PostSerializer(serializers.ModelSerializer):
         featured_image = self.context['request'].FILES.get('featured_image')
         if featured_image:
             instance.featured_image = featured_image
-        return super().update(instance, validated_data)
+
+        # Optional: handle manual M2M updates here if needed
+        tags_data = self.context['request'].data.getlist('tags')
+        if tags_data:
+            instance.tags.set(tags_data)
+
+        # Update the rest of the fields using validated_data
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+            
+        instance.save()
+        return instance
 
 
