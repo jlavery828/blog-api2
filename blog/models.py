13e6dbc64b45b12276452312fa9cb7bd83911b27
Jason <jlavery828@gmail.com>
2025-08-06 10:52:45 -0400

    added self.image.name to PostImage __str__ method

--- blog/models.py
+++ blog/models.py
@@ -54,6 +54,9 @@ class PostImage(models.Model):
     image = models.ImageField(upload_to='post_images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.image.name
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
