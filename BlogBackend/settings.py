bd00db3049faca16ca9c51db2548884a021a192a
Jason <jlavery828@gmail.com>
2025-08-16 19:32:11 -0400

    added fly.dev to CORS_ALLOWED_ORIGINS

--- BlogBackend/settings.py
+++ BlogBackend/settings.py
@@ -171,6 +171,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # SvelteKit dev server
+    "https://blog-daisyui2.fly.dev",  # Sveltekit online frontend
 ]
 
 
