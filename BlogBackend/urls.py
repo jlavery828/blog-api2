1d7f226551cdd3a089d5a45007f4be308c9fa3a0
Jason <jlavery828@gmail.com>
2025-08-21 11:26:49 -0400

    removed import for CookieAuth

--- BlogBackend/urls.py
+++ BlogBackend/urls.py
@@ -13,7 +13,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
-from blog.views import SimpleTokenObtainPairView, CookieJWTAuthentication
+from blog.views import SimpleTokenObtainPairView
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
